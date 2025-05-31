469b7658215b50bf6ea5c0751b76994e29c66ea9
Matěj Štágl <stagl.matej@centrum.cz>
2025-05-31 11:46:58 +0200

    setup project

---
+++ MimeTypeCore/MimeTypeCore.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace MimeTypeCore.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CollisionTs()
+    {
+        using FileStream streamVideo = File.Open("files/video.ts", FileMode.Open);
+        using FileStream streamTypescript = File.Open("files/typescript.ts", FileMode.Open);
+
+        MimeTypeMap.TryGetMimeType(streamVideo, out string mimeTypeVideo);
+        MimeTypeMap.TryGetMimeType(streamTypescript, out string mimeTypeTypescript);
+        
+        Assert.That(mimeTypeVideo, Is.EqualTo("video/mpeg"));
+        Assert.That(mimeTypeTypescript, Is.EqualTo("text/typescript"));
+    }
+}
