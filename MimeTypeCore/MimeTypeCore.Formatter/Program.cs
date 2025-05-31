ca9628652185d6654499b6623858584957a04acf
Matěj Štágl <stagl.matej@centrum.cz>
2025-05-31 13:39:16 +0200

    setup formatter

--- MimeTypeCore/MimeTypeCore.Formatter/Program.cs
+++ MimeTypeCore/MimeTypeCore.Formatter/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        // DictionaryFormatter.SortDictionaryEntries(filePath);
+        string path = "../../../../MimeTypeCore/MimeTypeMap.cs";
+        
+        if (File.Exists(path))
+        {
+            DictionaryFormatter.SortDictionaryEntries(path);   
+        }
     }
 }
